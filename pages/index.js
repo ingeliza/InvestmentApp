1b6d7e37a5b8dde0b9447b179db07b0dd20c5691
Ingela <ingelaelizabeth.rohusaar@tptlive.ee>
2022-08-30 16:58:35 +0300

    add prisma

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,17 @@
-import Head from 'next/head'
+import excuteQuery from '../lib/db'
 
-export default function Home() {
-  return (
-    <div>
-      <p>Create stock</p>
-
-      
-    </div>
-  )
-}
+export default async (req, res) => {
+  console.log(process.env.GREETING)
+  //   try {
+  //       console.log("req nom", req.body)
+  //     const result = await excuteQuery({
+  //         query: 'INSERT INTO post(content) VALUES(?)',
+  //         values: [req.body.content],
+  //     });
+  //     console.log( "ttt",result );
+  // } catch ( error ) {
+  //     console.log( error );
+  // }
+  
+  
+  };
